753a800ba6019b432968e0ab02c0b0e6548670c5
Ziqi Liu <ziqilau@gmail.com>
2015-06-10 01:19:00 -0400

    an example of script

--- src/run.py
+++ src/run.py
@@ -1,15 +1,19 @@
 alg = 'mf'
-traindata='../data/netflix_train_10blocks'
-testdata='../data/netflix_test_1blocks'
-validdata='../data/netflix_test_1blocks'
-it=15
 nu = 480189
 nv = 17770
+traindata='../data/netflix_train_10blocks'
+testdata='../data/netflix_test_1blocks'
+
+it=15
 fly=8
-mineta=2e-13
 lam=5e-3
+
 eps=0.0
+tau=0
+mineta=2e-13
+
 eta_reg=2e-3
+validdata='../data/netflix_test_1blocks'
 
 import os
 import sys
@@ -19,8 +23,8 @@ for eta in [2e-2]:
   for temp in [1e-1]:
     for gam in [1.0]:
       for dim in [16]:
-        print './mf --alg %s --mineta %e --epsilon %f --fly %d --gam %f --train %s --test %s --valid %s --eta_reg %e --result %s --eta %e --lambda %e --iter %d --dim %d --nu %d --nv %d'%(alg,mineta,eps,fly,gam,traindata,testdata,validdata,eta_reg,'%s_dim%d'%(alg,dim),eta,lam,it,dim,nu,nv)
+        print './mf --alg %s --train %s --test %s --valid %s --nu %d --nv %d --eta %e --lambda %e --gam %f --result %s --iter %d --dim %d --fly %d --epsilon %f --tau %d --temp %e --mineta %e --eta_reg %e'%(alg,traindata,testdata,validdata,nu,nv,eta,lam,gam,'%s_dim%d'%(alg,dim),it,dim,fly,eps,tau,temp,mineta,eta_reg)
         sys.stdout.flush()
-        os.system('./mf --alg %s --mineta %e --epsilon %f --fly %d --gam %f --train %s --test %s --valid %s --eta_reg %e --result %s --eta %e --lambda %e --iter %d --dim %d --nu %d --nv %d'%(alg,mineta,eps,fly,gam,traindata,testdata,validdata,eta_reg,'%s_dim%d'%(alg,dim),eta,lam,it,dim,nu,nv))
+        os.system('./mf --alg %s --train %s --test %s --valid %s --nu %d --nv %d --eta %e --lambda %e --gam %f --result %s --iter %d --dim %d --fly %d --epsilon %f --tau %d --temp %e --mineta %e --eta_reg %e'%(alg,traindata,testdata,validdata,nu,nv,eta,lam,gam,'%s_dim%d'%(alg,dim),it,dim,fly,eps,tau,temp,mineta,eta_reg))
         sys.stdout.flush()
 
